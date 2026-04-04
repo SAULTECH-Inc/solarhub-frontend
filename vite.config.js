d5e18f30df95af4e1a481d4963185c4670f8bc51
Shadrach Adamu <shadrachadamuul@gmail.com>
2026-04-05 00:24:14 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -34,6 +34,7 @@ export default defineConfig({
         theme_color: '#0a0a0a',
         background_color: '#0a0a0a',
         display: 'standalone',
+        start_url: '/',
         icons: [
           {
             src: 'pwa-192x192.png',
